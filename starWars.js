e87068b218a8b13098c14e7724c8c2ac7816fc08
Puyan Wei <puyanwei@gmail.com>
2018-06-28 11:46:57 +0100

    Returned names of chosen film

--- starWars.js
+++ starWars.js
@@ -1,17 +1,36 @@
 function run(film, character) {
     // code goes here
+    charactersOfFilm(film);
+}
+
+function charactersOfFilm(film) {
+    let characters = [];
+    let title;
+    requestData('https://swapi.co/api/films/').then((response) => {
+        response.data.results.map((element, index) => {
+            if (element.title === film) {
+                element.characters.map((element) => {
+                    requestData(element).then((response) => {
+                        console.log(
+                            'response ' + JSON.stringify(response.data.name)
+                        );
+                    });
+                    // characters.push(requestData(element.characters));
+                });
+            }
+        });
+    });
 }
 
 function requestData(url) {
-    // Make a request for a user with a given ID
-    axios
+    return axios
         .get(url)
         .then((response) => {
-            console.log(response);
+            return response;
         })
         .catch((error) => {
             console.log(error);
         });
 }
 
-requestData('https://swapi.co/api/films/');
+charactersOfFilm('Return of the Jedi');
